489b390890b86bd46f4cf3591eaceb654342d303
Nerdlin <nerdlinnut@gmail.com>
2024-11-14 17:17:27 +0500

    Initial commit

--- fin_crm/settings.py
+++ fin_crm/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fin_crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bot_base',
+        'USER': 'postgres',
+        'PASSWORD': '1194',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
